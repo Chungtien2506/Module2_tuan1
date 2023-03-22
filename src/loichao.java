884980de812f06aa0d7e59e5ea456a2ba64bca6f
Chungtien2506 <chungcoiaaaaa@gmail.com>
2023-03-22 17:17:43 +0700

    xong

---
+++ src/loichao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class loichao {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Moi ban nhap ten");
+        String name = sc.nextLine();
+        System.out.println("hello: " + name);
+
+    }
+}
+
+
+
